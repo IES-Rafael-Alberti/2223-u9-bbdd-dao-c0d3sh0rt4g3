5185ce23230e33c18626eb11e966ccb4274e6f53
Erik López Jiménez <eriking2004@gmail.com>
2023-05-04 02:25:14 +0200

    Added ProgramActionsDAO() class

---
+++ src/main/kotlin/ProgramActionsDS.kt
@@ -0,0 +1,13 @@
+class ProgramActionsDS: ProgramActionsInterface {
+    override fun addCompetitor(command: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteCompetitor(command: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showCompetitor(command: String): List<Group> {
+        TODO("Not yet implemented")
+    }
+}
